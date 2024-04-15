65ff003c46eb4fceb729f290d44c950050d76368
vijayzite <vijayzite101@gmail.com>
2024-04-15 11:25:49 +0530

    Commit

--- Eklavya_elearning Platform/ElearningBackend(src)/ElearningBackend/src/main/java/com/backend/elearning/models/OrderDetails.java
+++
@@ -1,5 +0,0 @@
-package com.backend.elearning.models;
-
-public class OrderDetails {
-
-}
--- Eklavya_elearning Platform/ElearningBackend(src)/ElearningBackend/src/main/java/com/backend/elearning/services/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.backend.elearning.services;
-
-import com.backend.elearning.models.Order;
-
-public interface OrderService {
-
-	public String saveOrder(Order order);
-
-}
